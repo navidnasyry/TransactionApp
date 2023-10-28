3611a450e4b53f67135eaa963fb2a14085a342a7
Navid Nasyry <navidnasyry@gmail.com>
2023-10-28 12:21:00 +0330

    fix: space and newline

--- TransactionAppProject/TransactionAppProject/Program.cs
+++ TransactionAppProject/TransactionAppProject/Program.cs
@@ -12,8 +12,8 @@ var elasticClient = elasticObj.GetElasticsearchClient();
 // Start WebApplication Setup
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
